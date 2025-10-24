14e4c583448f40f12a5afa2495bb11082f7775d7
cueavyqwp <127200270+cueavyqwp@users.noreply.github.com>
2025-10-25 01:46:07 +0800

    更新校验相关内容

--- src/pmccc/verify.py
+++ src/pmccc/verify.py
@@ -1,33 +1,66 @@
 """
-校验类
+校验相关
 """
 
-__all__ = ["verify"]
+__all__ = ["TYPES", "HASHER", "get_type", "hasher", "verify_hash"]
 
 import hashlib
 import typing
+import enum
+import os
 
 
-class verify:
+class TYPES(enum.Enum):
 
-    def __init__(self, value: str, type: typing.Literal["sha1", "sha256", "sha512", "md5"]) -> None:
-        """
-        用于校验文件
+    SHA1 = 0
+    SHA256 = 1
+    SHA512 = 2
+    MD5 = 3
 
-        ---
 
-        value: 哈希值/MD5
-        """
-        self.value = value
-        self.hash = {"sha1": hashlib.sha1, "sha256": hashlib.sha256,
-                     "sha512": hashlib.sha512, "md5": hashlib.md5}.get(type, hashlib.sha1)()
+HASHER = {
+    TYPES.SHA1: hashlib.sha1,
+    TYPES.SHA256: hashlib.sha256,
+    TYPES.SHA512: hashlib.sha512,
+    TYPES.MD5: hashlib.md5
+}
 
-    def update(self, data: str | bytes) -> "verify":
+
+def get_type(value: str) -> TYPES:
+    return {32: TYPES.MD5, 40: TYPES.SHA1, 64: TYPES.SHA256, 128: TYPES.SHA512}[len(value)]
+
+
+class hasher:
+
+    def __init__(self, hasher: TYPES) -> None:
+        self.hash = HASHER[hasher]()
+
+    def update(self, data: str | bytes) -> None:
         self.hash.update(data.encode() if isinstance(data, str) else data)
-        return self
 
+    def load(self, file: str) -> str:
+        with open(file, "rb") as fp:
+            for data in iter(lambda: fp.read(4096), b""):
+                self.update(data)
+        return self.hexdigest
+
+    def load_dir(self, path: str, filter: typing.Optional[str] = None) -> str:
+        for file in sorted(os.listdir(path)):
+            file = os.path.join(path, file)
+            if os.path.isdir(file) or (filter is not None and filter not in file):
+                continue
+            self.load(file)
+        return self.hexdigest
+
+    @property
     def hexdigest(self) -> str:
         return self.hash.hexdigest()
+
+
+class verify_hash(hasher):
+    def __init__(self, value: str) -> None:
+        super().__init__(get_type(value))
+        self.value = value
 
     def check(self) -> bool:
         return self.hash.hexdigest() == self.value
